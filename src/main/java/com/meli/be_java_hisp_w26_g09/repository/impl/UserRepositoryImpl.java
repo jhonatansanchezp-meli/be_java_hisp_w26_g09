4061c9820697f9bc12bc251026165f861725b711
jgomezreyes <joan.gomezreyes@mercadolibre.com.co>
2024-04-23 15:44:59 -0500

    updated repository

---
+++ src/main/java/com/meli/be_java_hisp_w26_g09/repository/impl/UserRepositoryImpl.java
@@ -0,0 +1,34 @@
+package com.meli.be_java_hisp_w26_g09.repository.impl;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.meli.be_java_hisp_w26_g09.entity.Post;
+import com.meli.be_java_hisp_w26_g09.entity.Product;
+import com.meli.be_java_hisp_w26_g09.entity.User;
+import com.meli.be_java_hisp_w26_g09.repository.IUserRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class UserRepositoryImpl implements IUserRepository {
+    List<User> listOfUser = new ArrayList<>();
+
+    public UserRepositoryImpl() throws IOException {
+        loadDataBase();
+    }
+
+    private void loadDataBase() throws IOException {
+        File file;
+        ObjectMapper objectMapper = new ObjectMapper();
+        List<User> users ;
+
+        file= ResourceUtils.getFile("classpath:users_generated.json");
+        users= objectMapper.readValue(file,new TypeReference<List<User>>(){});
+        listOfUser = users;
+    }
+}
